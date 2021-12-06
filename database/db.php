9a4b9de67a02d67cf46cb98bf84532ec473ccfc2
ichsanrazan <ichsanrazan33@gmail.com>
2021-12-06 12:38:48 +0700

    configure database

--- database/db.php
+++ database/db.php
@@ -1,15 +1,12 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$database = "berkah_komputer";
-
-$db = new mysqli($host, $username, $password, $database);
-
-// if($db->connect_error){
-//     echo "koneksi gagal";
-// }else{
-//     echo "koneksi berhasil";
-// }
-
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$db_host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "cv_db";
-
-$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
-
-if(mysqli_connect_error()){
-	echo 'Gagal melakukan koneksi ke Database : '.mysqli_connect_error();
-}
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
